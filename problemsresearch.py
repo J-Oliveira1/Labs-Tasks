13c500825e4ba3fa27d3cb0c05eae087d20703f1
unknown <j.poliveira90@outlook.com>
2022-12-13 14:34:44 -0500

    New File

---
+++ problemsresearch.py
@@ -0,0 +1,14 @@
+# Problem Solving 1-
+# Task 1- 
+
+# favorite_number = 20
+
+# Task 2-
+import random
+
+favorite_number = 20
+
+random_number = random.randrange(25)
+print(random_number)
+
+while random_number == (20)
